6d441aa0140bd594f023dafcb8bfad4ca0ced0ee
yazop <vladpopov9@mail.ru>
2024-04-04 23:09:04 +0300

    Провел исследования в примении хранилища эмбедингов, QnA, ретривера.

--- text_generator.py
+++ text_generator.py
@@ -1,6 +1,5 @@
 from langchain.schema import HumanMessage, SystemMessage
 from langchain.prompts import PromptTemplate
-from langchain.output_parsers import CommaSeparatedListOutputParser
 from langchain_community.chat_models import GigaChat
 from aiogram.types import Message
 from os import getenv
@@ -8,14 +7,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class TextGen(object):
 
+class TextGen(object):
     def __init__(self, credentials, ssl_cert, scope, profanity):
-        self.chat = GigaChat(credentials=credentials, verify_ssl_certs=ssl_cert, scope=scope, profanity=profanity)
-    
+        self.chat = GigaChat(
+            credentials=credentials,
+            verify_ssl_certs=ssl_cert,
+            scope=scope,
+            profanity=profanity,
+        )
 
     def answer(self, message: Message) -> None:
-
         prompt = PromptTemplate(
             template="Ответь на поставленный вопрос: {subject}",
             input_variables=["subject"],
@@ -24,7 +26,12 @@ class TextGen(object):
         output = self.chat(
             [
                 SystemMessage(
-                    content="Ты профессиональный ассистент Российского государственного гидрометеорологического университета, который помогает студентам и преподавателям решать их проблемы. Ты должен общаться в уважительном тоне. Пиши только по существу."
+                    content="""Ты профессиональный ассистент Российского
+                    государственного гидрометеорологического университета,
+                    который помогает студентам и преподавателям решать их
+                    проблемы: навигация внутри университета, ответ на общие
+                    вопросы, касающиеся РГГМУ. При ответе на вопросы общайся
+                    в деловом стиле и пиши только по существу."""
                 ),
                 HumanMessage(content=prompt.format(subject=message)),
             ]
@@ -34,6 +41,6 @@ class TextGen(object):
 
 
 talkbox = TextGen(getenv("CREDENTIAL"), False, getenv("SCOPE"), True)
-#a = talkbox.answer("Привет, что ты умеешь?")
-#print(a)
-#print(type(a))
+# a = talkbox.answer("Привет, что ты умеешь?")
+# print(a)
+# print(type(a))
